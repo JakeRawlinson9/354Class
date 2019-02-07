b79941545f8c05a378b88956499f9937633c4f25
Wilhelm <u16056885@tuks.co.za>
2019-02-07 11:13:28 +0200

    Update Site.js

--- Site.js
+++ Site.js
@@ -1,3 +1,8 @@
+function first()
+{
+  alert("I am looking forward to INF 354 to become competent in the industry one day.")
+}
+
 function second()
 {
   alert("I am looking forward to INF 354 to create mobile apps and MVC Web apps");
